c628bb7c84d54557510246755f2ed0155a1655da
Burak AÇIK <burakacik@outlook.com>
2022-08-16 16:00:39 +0300

    Network Request ve Codable Yapısı Eklendi

--- Flickr Client App/Screens/RecentPhotos/Controller/RecentPhotosTableViewController.swift
+++ Flickr Client App/Screens/RecentPhotos/Controller/RecentPhotosTableViewController.swift
@@ -9,13 +9,59 @@ import UIKit
 
 class RecentPhotosTableViewController: UITableViewController, UISearchResultsUpdating {
     
+    private var response: PhotosResponse? {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSearchController()
+        fetchRecentPhotos()
+        
+        
+        
+    }
+    
+    private func setupSearchController() {
         let search = UISearchController(searchResultsController: nil)
         search.searchResultsUpdater = self
         search.obscuresBackgroundDuringPresentation = false
         search.searchBar.placeholder = "Type something here to search"
         navigationItem.searchController = search
+    }
+    
+    private func fetchRecentPhotos() {
+        guard let url = URL(string:
+                                "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=7dd89640e14551c5df5f7bbae1f68223&format=json&nojsoncallback=1&extras=date_upload,date_taken,owner_name,icon_server,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o") else { return }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            if let data = data, let response = try? JSONDecoder().decode(PhotosResponse.self, from: data) {
+                self.response = response
+            }
+        }.resume()
+    }
+    
+    private func searchPhotos(with text: String) {
+        guard let url = URL(string:
+                                "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=7dd89640e14551c5df5f7bbae1f68223&text=flower&format=json&nojsoncallback=1&extras=date_upload,date_taken,owner_name,icon_server,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o") else { return }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            if let data = data, let response = try? JSONDecoder().decode(PhotosResponse.self, from: data) {
+                self.response = response
+            }
+        }.resume()
     }
     
     // MARK: - UITableViewDataSource & UITableViewDelegate
@@ -54,7 +100,7 @@ class RecentPhotosTableViewController: UITableViewController, UISearchResultsUpd
     func updateSearchResults(for searchController: UISearchController) {
         guard let text = searchController.searchBar.text else { return }
         if text.count > 2 {
-            print(text)
+            searchPhotos(with: text)
         }
     }
 }
